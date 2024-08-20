2f90ed63c0d11907360e09ac7d8e1895141914e3
= <=>
2024-08-20 08:45:04 -0500

    adding new controllers and models

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,7 @@
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const { errorCode, errorMessage } = require("../utils/errors");
 
@@ -31,14 +35,39 @@ const getUser = (req, res) => {
 
 // Controller to create a new user
 const createUser = async (req, res) => {
-  const { name, avatar } = req.body;
-  if (!name || !avatar){
-    return res.status(errorCode.invalidData).send({ message:errorMessage.invalidData})
+  const { name, avatar, email, password } = req.body;
+
+  if (!email) {
+    return res.status(errorCode.invalidData).send({ message: errorMessage.invalidEmail })
   }
-  return User.create({ name, avatar })
-    .then((user) => res.status(201).send({data: user}))
+
+  if (!validator.isEmail(email)) {
+    return res
+      .status(errorCode.invalidData)
+      .send({ message: errorMessage.invalidEmail });
+  }
+  return User.findOne({ email })
+    .then((existingEmail) => {
+      if (existingEmail) {
+        throw new Error('Email already exists');
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) => User.create({ name, avatar, email, password: hash }))
+    .then((user) =>
+      res.send({
+        name: user.name,
+        avatar: user.avatar,
+        email: user.email,
+      })
+    )
     .catch((e) => {
       console.error(e);
+      if (e.message === 'This Email already Exists') {
+        return res
+          .status(errorCode.conflict)
+          .send({ message: errorMessage.existEmail });
+      }
       if (e.name === 'ValidationError') {
         return res.status(errorCode.invalidData).send({ message: errorMessage.validationError });
       }
@@ -46,4 +75,37 @@ const createUser = async (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUser, createUser };
+
+// Controller to Login
+const login = (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(errorCode.invalidData)
+      .send({ message: errorMessage.requiredEmailAndPassword });
+  }
+  if (!validator.isEmail(email)) {
+    return res
+      .status(errorCode.invalidData)
+      .send({ message: errorMessage.invalidEmail });
+  }
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: '7d',
+      });
+      return res.send({ token });
+    })
+    .catch((e) => {
+      if (e.message === 'Incorrect email or password') {
+        return res
+          .status(errorCode.unauthorized)
+          .send({ message: errorMessage.incorrectEmailOrPassword });
+      }
+      return res
+        .status(errorCode.defaultError)
+        .send({ message: errorMessage.defaultError });
+    });
+};
+
+module.exports = { getUsers, getUser, createUser, login };
--- models/user.js
+++ models/user.js
@@ -20,7 +20,46 @@ const userSchema = new mongoose.Schema({
             message: "You must enter a valid URL",
         },
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator(value) {
+                return validator.isEmail(value);
+            },
+            message: 'You must enter a valid email',
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        select: false,
+    },
+
 });
+
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+    // trying to find the user by email
+    return this.findOne({ email }) // this — the User model
+        .select("+password")
+        .then((user) => {
+            // not found - rejecting the promise
+            if (!user) {
+                return Promise.reject(new Error(errorMessage.incorrectEmailOrPassword));
+            }
+            return bcrypt.compare(password, user.password).then((matched) => {
+                if (!matched) {
+                    return Promise.reject(new Error(errorMessage.incorrectEmailOrPassword));
+                }
+                return user;
+            });
+        });
+
+};
+
 
 // User Model
 module.exports = mongoose.model('User', userSchema);
