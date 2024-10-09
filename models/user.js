20ec82a544c5b1da18a98acf0b031bd9090b933a
= <=>
2024-10-08 23:32:28 -0500

    rendering code

--- models/user.js
+++ models/user.js
@@ -42,24 +42,24 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-    // trying to find the user by email
-    return this.findOne({ email }) // this — the User model
-        .select("+password")
-        .then((user) => {
-            // not found - rejecting the promise
-            if (!user) {
-                return Promise.reject(new Error(errorMessage.incorrectEmailOrPassword));
-            }
-            return bcrypt.compare(password, user.password).then((matched) => {
-                if (!matched) {
-                    return Promise.reject(new Error(errorMessage.incorrectEmailOrPassword));
-                }
-                return user;
-            });
+userSchema.statics.findUserByCredentials = function findUserbyCredentials(
+    email,
+    password
+  ) {
+    return this.findOne({ email })
+      .select("+password")
+      .then((user) => {
+        if (!user) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+        return bcrypt.compare(password, user.password).then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error("Incorrect email or password"));
+          }
+          return user;
         });
-
-};
+      });
+  };
 
 
 // User Model
