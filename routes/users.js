ea44e1ad89426268c5d25e58fbb81716e9e28bf9
= <=>
2024-11-14 12:13:17 -0600

    refactored

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require('../middlewares/auth');
-const { validateUpdateBody } = require("../middlewares/validation");
+const { validateUpdateUser } = require("../middlewares/validation");
 
 router.use(auth);
 
 router.get('/me', getCurrentUser);
-router.patch("/me", validateUpdateBody, updateUser);
+router.patch("/me", validateUpdateUser, updateUser);
 
 
 
